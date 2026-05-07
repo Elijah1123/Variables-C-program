3bd931deb92dbf6de908f4e86ed72e75aa21ed2a
Elijah1123 <elijahmzalendo659@gmail.com>
2026-05-07 14:03:12 +0300

    interger

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+
+    int age = 25;
+
+    int year =  2026;
+
+    int km = 6;
+
+    printf("You are %d years old \n", age);
+    printf("We are on 21`st century on %d year\n", year);
+    
+    printf("I walk almost %d Km per day", km);
+
+
+
+
+
+    return 0;
+}
