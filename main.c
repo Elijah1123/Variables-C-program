c72327b9725f3d4e95f446ae6d34de2534f2d7fc
Elijah1123 <elijahmzalendo659@gmail.com>
2026-05-07 14:24:27 +0300

    float-C

--- main.c
+++ main.c
@@ -8,6 +8,15 @@ int main() {
 
     int km = 6;
 
+    float  gpa = 2.5;
+    float price = 99.99;
+    double pi = 3.14124886794;
+
+
+    printf("The value of pi is %.15lf\n", pi);
+    printf("The price is $%.2f\n", price);
+    printf("Your were gpa is %f\n", gpa);
+
     printf("You are %d years old \n", age);
     printf("We are on 21`st century on %d year\n", year);
     
